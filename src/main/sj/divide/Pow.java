ff0fcf607b782141e90522440f644c81e3ef34f7
steven <shijia0620@126.com>
2018-03-21 21:51:37 +0800

    kmp

---
+++ src/main/sj/divide/Pow.java
@@ -0,0 +1,23 @@
+package sj.divide;
+
+/**
+ * Created by steven on 18-3-19 下午9:18
+ * shijia0620@126.com
+ * 自己实现pow指数函数
+ */
+public class Pow {
+    public double pow(int base, int n ){
+        if(n<0) return 1/pow(base,-n);
+        if(n == 0) return 1;
+        double halfNum = pow(base,n>>1);
+        int res = (n % 2 == 0)?1:base;
+        return halfNum*halfNum*res;
+    }
+
+    public static void main(String[] args) {
+        Pow pow = new Pow();
+        for(int i=-3;i<5;i++){
+            System.out.println(pow.pow(2,i));
+        }
+    }
+}
